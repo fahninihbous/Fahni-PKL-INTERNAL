c8f355c2d6969956bb9d1d155354b08ed842daa5
Putra <Galihramadami.com>
2025-12-15 11:53:16 +0700

    latihan_hari_1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+// Route tentang
+Route::get('/tentang', function () {
+    return view('tentang');
+});
+// route di isi manual nama nya
+Route::get('/sapa/{nama}', function ($nama) {
+    return "Halo, $nama! Selamat datang di Toko Online.";
+});
+// route dengan default parameter antisipasi kalo ga di isi
+Route::get('/kategori/{nama?}', function ($nama = 'Semua') {
+    return "Menampilkan kategori: $nama";
+});
+Route::get('/produk/{id}', function ($id) {
+    return "Detail produk #$id";
+})->name('produk.detail');
